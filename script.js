6adba3eb4494fb7dbc4486e24dc685f47de86d0a
unknown <lydia.b.mclaughlin@gmail.com>
2020-11-11 20:05:07 -0500

    hover and click functionality

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ startGame()
 function startGame() {
     Array.from(cells).forEach(cell => {
         cell.addEventListener('click', handleClick, { once: true})
+        cell.addEventListener('mouseover', handleMouseover)
     })
 }
 
@@ -14,14 +15,35 @@ function handleClick(e) {
     drawMark(cell);
 }
 
+function handleMouseover(e) {
+    const cell = e.target;
+    shadowMark(cell);
+}
+
 function drawMark(cell) {
     if (currentPlayer === 'x') {
+        cell.classList.remove('xhover')
         cell.classList.add('x')
         currentPlayer = 'o'
     } else {
+        cell.classList.remove('ohover')
         cell.classList.add('o')
         currentPlayer = 'x'
     }
+    checkWin()
+}
+
+function shadowMark(cell) {
+    if (currentPlayer === 'x' && !cell.classList.contains('x') && !cell.classList.contains('o')) {
+        cell.classList.remove('ohover')
+        cell.classList.add('xhover')
+    } else if (currentPlayer === 'o' && !cell.classList.contains('x') && !cell.classList.contains('o')) {
+        cell.classList.remove('xhover')
+        cell.classList.add('ohover')
+    }
+}
+
+function checkWin() {
 }
 
 
